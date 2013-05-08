1af0bbd3676bd04c4cfb52f1e7658634b1c0ae31
Vincent Shi <winks716@163.com>
2013-05-08 13:08:06 +0800

    20130508 home combine4
    Add new javascript function for alertDiv post data: Init,
    checkDataBeforePost, postData, resultHandle
    To do, update the alertDiv init function to create div#alertDiv
    dynamically, and replace the post data action in all template files.

--- js/global.js
+++ js/global.js
@@ -1,3 +1,80 @@
-$(document).ready(function(){
-	//$('#core').tabs();
-})
+/* #alertDiv data handling function set 
+ * Need jQuery support
+ * Start */
+function alertDivInit(){
+	$('#alertDiv').hide().dialog({
+		'modal':true,
+		'autoOpen':false
+	}).ajaxStart(function(){
+		$(this).dialog('open');
+	});
+}
+function alertDivPostData(postOptions){
+	var defaultOptions = {
+			postUrl: '#',
+			postData: {},
+			postConfirm: '',
+			redirectUrl: '',
+			tipAfterPost: '正在刷新页面，请稍候……',
+			checkBeforePost: '',
+			tipBeforePost: '请将数据填写完整后再次递交'
+	};
+	var o = $.extend(defaultOptions, postOptions);
+	//if need confirm before post
+	if(o.postConfirm!=''){
+		if(confirm(o.postConfirm)){
+			//if need data precheck before post
+			if(o.checkBeforePost!=''){
+				//if passed the data precheck
+				if(alertDivCheckBeforePost(o.checkBeforePost)){
+					$.post(o.postUrl,o.postData,function(data){
+						alertDivHandleResult(data, o.redirectUrl, o.tipAfterPost);
+					});
+				}else{
+					alert(o.tipBeforePost);
+				}
+			}else{
+				$.post(o.postUrl,o.postData,function(data){
+					alertDivHandleResult(data, o.redirectUrl, o.tipAfterPost);
+				});
+			}
+		}
+	}else{
+		if(o.checkBeforePost!=''){
+			if(alertDivCheckBeforePost(o.checkBeforePost)){
+				$.post(o.postUrl,o.postData,function(data){
+					alertDivHandleResult(data, o.redirectUrl, o.tipAfterPost);
+				});
+			}else{
+				alert(o.tipBeforePost);
+			}
+		}else{
+			$.post(o.postUrl,o.postData,function(data){
+				alertDivHandleResult(data, o.redirectUrl, o.tipAfterPost);
+			});
+		}
+	}
+}
+function alertDivCheckBeforePost(data){
+	var result = eval('(' + data + ')');
+	if(result){
+		return true;
+	}else{
+		return false;
+	}
+}
+function alertDivHandleResult(data, redirectUrl, tipAfterPost){
+	var json = eval('(' + data + ')');
+	if(json.httpstatus = 200){
+		alert(json.msg);
+	}else{
+		alert(json.error);
+	}
+	$('#alertDiv').html(tipAfterPost);
+	if(redirectUrl!=''){
+		window.location = redirectUrl;
+	}else{
+		window.location.reload();
+	}
+}
+/* #alertDiv data handling function set End */
--- modules/sales/activity.php
+++ modules/sales/activity.php
@@ -1,14 +1,49 @@
 <?php
 switch($A){
 	case 'record':
+	    $myId = $_SESSION['employee_id'];
+	    $assign['mycustomer'] = basicMysqliQuery('uatme_oa_sales_customer', 
+	                ' WHERE ifavailable="1" AND (owner_employee_id LIKE "" 
+	            OR owner_employee_id="0" OR owner_employee_id LIKE "'.$myId.'" 
+	            OR owner_employee_id LIKE "%,'.$myId.'" 
+	            OR owner_employee_id LIKE "'.$myId.',%" 
+	            OR owner_employee_id LIKE "%,'.$myId.',%")');
+	    $customer = basicMysqliQuery('uatme_oa_sales_customer', ' WHERE ifavailable="1"');
+	    $activity = basicMysqliQuery('uatme_oa_sales_activity', ' WHERE employee_id="'.$_SESSION["employee_id"].'"');
+	    foreach($activity as $a){
+	        $a['customerName'] = $customer[$a['customer_id']]['name'];
+	        $assign['activity'][] = $a;
+	    }
 		$smarty->assign($assign);
 		$smarty->display('sales/activity.record.html');
 		break;
 	case 'save':
-		
+		$date = $_POST['date'];
+		$start = $_POST['start'];
+		$end = $_POST['end'];
+		$customerId = $_POST['customerId'];
+		$detail = $_POST['detail'];
+		$minday = Date('Y-m-d',(time()-Date('w')*24*3600));
+		$maxday = Date('Y-m-d',(time()+(6-Date('w'))*24*3600));
+		if($date >= $minday and $date <= $maxday){
+		    $sql = 'INSERT INTO uatme_oa_sales_activity(date, start, end, customer_id, detail, employee_id)
+		             VALUES ("'.$date.'", "'.$start.'", "'.$end.'", "'.$customerId.'", "'.$detail.'", "'.$_SESSION["employee_id"].'")';
+		    $mysqli->query($sql);
+		    $httpstatus = 200;
+		    $msg = '活动记录成功！';
+		}else{
+		    $httpstatus = 500;
+		    $error = '记录日期 ('.$date.') 超出允许范围 ('.$minday.'~'.$maxday.')';
+		}
+		sendResponse($httpstatus, $error, $msg);
 		break;
 	case 'delete':
-		
+		$id = $_POST['id'];
+		$sql = 'DELETE FROM uatme_oa_sales_activity WHERE id="'.$id.'"';
+		$mysqli->query($sql);
+		$httpstatus = 200;
+		$msg = '记录成功删除'.$sql;
+		sendResponse($httpstatus, $error, $msg);
 		break;
 	default:
 		
