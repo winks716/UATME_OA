e00085810fa86c11ccb474781da203af5fd1052a
Vincent.Shi <uatmestudio@163.com>
2013-07-01 18:00:19 +0800

    20130701 office combine
    Continued to start the checkin project.
    Basically done the basic structure of checkin infor page, add a new data
    table uatme_oa_hr_employee_basic_info.
    Added some basic page for employee information editing.

--- modules/hr/checkin.php
+++ modules/hr/checkin.php
@@ -1,14 +1,24 @@
 <?php
 switch($A){
-    case 'employee.basic':
-        $assign['employee'] = basicMysqliQuery('uatme_oa_system_employee', ' WHERE ifcheckin=0 AND ifavailable=0 ');
+    case 'employee.list':
+        $employee_id = (isset($_GET['eid']) && $_GET['eid']>0) ? $_GET['eid'] : 0;
+        $assign['employee_list'] = basicMysqliQuery('uatme_oa_system_employee', ' WHERE ifcheckedin=0 AND ifavailable=0 ');
+        $assign['employee_infor']['basic'] = basicMysqliQuery('uatme_oa_hr_employee_basic_info', ' WHERE employee_id="'.$employee_id.'"');
+        $assign['employee_infor']['education'] = basicMysqliQuery('uatme_oa_hr_employee_education_info', ' WHERE employee_id="'.$employee_id.'"');
+        $assign['employee_infor']['skill'] = basicMysqliQuery('uatme_oa_hr_employee_skill_info', ' WHERE employee_id="'.$employee_id.'"');
         $smarty->assign($assign);
-        $smarty->display('hr/employee.basic.html');
+        $smarty->display('hr/checkin.employee.list.html');
         break;
-    case 'employee.education':
-        
+    case 'employee.basic.edit':
+        $smarty->assign($assign);
+        $smarty->display('hr/checkin.basic.edit.html');
         break;
-    case 'employee.skill':
-        
+    case 'employee.education.edit':
+        $smarty->assign($assign);
+        $smarty->display('hr/checkin.education.edit.html');
+        break;
+    case 'employee.skill.edit':
+        $smarty->assign($assign);
+        $smarty->display('hr/checkin.skill.edit.html');
         break;
 }
