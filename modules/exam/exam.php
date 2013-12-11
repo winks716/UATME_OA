26325e51e820dc7edfca1f41a80f89455a2be1ee
Vincent.Shi <uatmestudio@163.com>
2013-12-11 17:44:41 +0800

    20131210 office combine

--- modules/exam/exam.php
+++ modules/exam/exam.php
@@ -7,17 +7,58 @@
 switch($A){
     case 'exam':
         $examToken = (isset($_GET['t']) && $_GET['t']!='') ? $_GET['t'] : '';
+        $examTemplate = 'exam/exam.exam.html';
         if($examToken){
             //find exam match the token, then show the exam
-            $sql = 'SELECT * FROM uatme_oa_oexam_exam_employee WHERE employeeId="'.$_SESSION['employee_id'].'" AND examToken="'.$examToken.'"';
-            $result = $mysqli->query($sql);
-            if($result->num_rows > 0){
-            	while($array = $result->fetch_assoc()){
-            		$assign['leave'][] = $array;
+            $sql = 'SELECT examId FROM uatme_oa_oexam_exam_employee WHERE employeeId="'.$_SESSION['employee_id'].'" AND examToken="'.$examToken.'"';
+            $resultPaper = $mysqli->query($sql);
+            if($resultPaper->num_rows == 1){
+            	while($arrayPaper = $resultPaper->fetch_assoc()){
+            		$assign['examId'] = $arrayPaper['examId'];
             	}
+            	
+            	//find related exam info, question info and option info
+            	//exam
+            	$sqlExam = 'SELECT * FROM uatme_oa_oexam_exam WHERE id="'.$assign['examId'].'"';
+            	$resultExam = $mysqli->query($sqlExam);
+            	if($resultExam->num_rows == 1){
+            	    while ($arrayExam = $resultExam->fetch_assoc()){
+            	        //question
+            	        $sqlQuestion = 'SELECT * FROM uatme_oa_oexam_question WHERE examId="'.$arrayExam['examId'].'"';
+            	        $resultQuestion = $mysqli->query($sqlQuestion);
+            	        if($resultQuestion->num_rows > 0){
+            	            while ($arrayQuestion = $resultQuestion->fetch_assoc()){
+            	                //option
+            	                $sqlOption = 'SELECT * FROM uatme_oa_oexam_option WHERE questionId="'.$arrayQuestion['id'].'"';
+            	                $resultOption = $mysqli->query($sqlOption);
+            	                if($resultOption->num_rows > 0){
+            	                    while ($arrayOption = $resultOption->fetch_assoc()){
+            	                        $arrayQuestion['option'][] = $arrayOption;
+            	                    }
+            	                }else{
+                                    $examTemplate = 'exam/exam.none.html';
+                                }
+            	                $arrayExam['question'][] = $arrayQuestion;
+            	            }
+            	        }else{
+                            $examTemplate = 'exam/exam.none.html';
+                        }
+            	        $assign['exam'] = $arrayExam;
+            	    }
+            	}else{
+                    $examTemplate = 'exam/exam.none.html';
+                }
+            	
+            }else{
+                $examTemplate = 'exam/exam.none.html';
             }
+        }else{
+            $examTemplate = 'exam/exam.none.html';
         }
 		$smarty->assign($assign);
-		$smarty->display('exam/exam.exam.html');
+		$smarty->display($examTemplate);
+        break;
+    case 'default':
+        
         break;
 }
